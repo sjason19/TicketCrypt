69ed6844ce53dba158e712f3fe0d5060bf249571
Patrick Lin <patricklin@alumni.ubc.ca>
2018-02-03 22:27:51 -0800

    rm template files

---
+++ src/containers/Controller.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export default class Controller extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      events: []
+    }
+  }
+
+  render () {
+    return (
+      <div />
+    )
+  }
+}
