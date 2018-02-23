8ab0c43b955b62e383f284f88493b0266aa7a590
Patrick Lin <patricklin@alumni.ubc.ca>
2018-02-22 20:35:29 -0800

    add blank home page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
+import Home from './Home'
 import EventCreator from './EventCreator'
 import MenuNavigator from './MenuNavigator'
 
@@ -8,6 +9,7 @@ const App = () => (
     <div>
       <MenuNavigator />
       <div className='ui grid stackable padded MainContentContainer'>
+        <Route exact path="/" component={Home} />
         <Route exact path='/event/new' component={EventCreator} />
       </div>
     </div>
