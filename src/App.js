b670bb5d9200158b741fae472a327278f63e2b51
Patrick Lin <patricklin@alumni.ubc.ca>
2018-02-22 20:19:23 -0800

    fix router

--- src/App.js
+++ src/App.js
@@ -1,28 +1,17 @@
-const React = require('react')
-const ReactDOM = require('react-dom')
+import React from 'react'
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
+import EventCreator from './EventCreator'
+import MenuNavigator from './MenuNavigator'
 
-const {
-  HashRouter: Router,
-  Route,
-  Link
-} = require('react-router-dom')
+const App = () => (
+  <Router>
+    <div>
+      <MenuNavigator />
+      <div className='ui grid stackable padded MainContentContainer'>
+        <Route exact path='/event/new' component={EventCreator} />
+      </div>
+    </div>
+  </Router>
+)
 
-const EventCreator = require('./EventCreator.js')
-const MenuNavigator = require('./MenuNavigator.js')
-
-class App extends React.Component {
-  render() {
-    return (
-      <Router>
-        <div>
-        <MenuNavigator />
-        <div className="ui grid stackable padded MainContentContainer">
-          <Route exact path="/event/new" component={EventCreator}/>
-        </div>
-        </div>
-      </Router>
-    )
-  }
-}
-
-module.exports = App;
+export default App
